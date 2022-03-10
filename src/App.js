e4128e2f31f481eb0169216eff52695a8b4d5536
Robin <62151722+IK-Robin@users.noreply.github.com>
2022-03-10 21:22:14 +0600

    git init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        deploy reactf
+        deploy react app
         </a>
       </header>
     </div>
